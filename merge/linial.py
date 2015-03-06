e1791150e43cfeaca54e3f9a486aa619ea759cb8
aureooms <aurelien.ooms@gmail.com>
2015-03-06 18:33:48 +0100

    fixes

--- merge/linial.py
+++ merge/linial.py
@@ -4,10 +4,9 @@ from information import partial , comparator
 
 def grid ( m , n ) :
 
-	e = mat( m + 1 , n + 1 )
+	e = mat( m + 1 , n + 1 , 0 )
 
-	for i in range ( m + 1 ) : e[i][0] = 1
-	for j in range ( n + 1 ) : e[0][j] = 1
+	e[0][0] = 1
 
 	return e
 
@@ -57,21 +56,25 @@ def linial ( compare , e , i , j ) :
 
 	"""
 
-	if e[i][j] is None :
+	if not e[i][j] :
 
-		result = compare( i - 1 , j - 1 )
-
-		if result > 0 :
-
-			e[i][j] = linial( compare , e , i - 1 , j )
-
-		elif result < 0 :
-
-			e[i][j] = linial( compare , e , i , j - 1 )
-
+		if   i == 0 : e[i][j] = linial( compare , e , i , j - 1 )
+		elif j == 0 : e[i][j] = linial( compare , e , i - 1 , j )
 		else :
 
-			e[i][j] = linial( compare , e , i - 1 , j ) + linial( compare , e , i , j - 1 )
+			result = compare( i - 1 , j - 1 )
+
+			if result > 0 :
+
+				e[i][j] = linial( compare , e , i - 1 , j )
+
+			elif result < 0 :
+
+				e[i][j] = linial( compare , e , i , j - 1 )
+
+			else :
+
+				e[i][j] = linial( compare , e , i - 1 , j ) + linial( compare , e , i , j - 1 )
 
 
 	return e[i][j]
